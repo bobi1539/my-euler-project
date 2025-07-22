1f988f830f00dee82ff30d4710db3f877168d025
Bobi Rival <bobirival@MacBook-Air-Bobi.local>
2025-07-22 17:19:01 +0700

    Update : add problem

---
+++ src/main/java/com/zero/euler/PowerDigitSum.java
@@ -0,0 +1,28 @@
+package com.zero.euler;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigInteger;
+
+@Slf4j
+public class PowerDigitSum {
+
+    public static void main(String[] args) {
+
+        BigInteger twoPowerThousand = BigInteger.TWO;
+
+        for (int i = 1; i < 1_000; i++) {
+            twoPowerThousand = twoPowerThousand.multiply(BigInteger.TWO);
+        }
+
+        log.info("2 power 1000 : {}", twoPowerThousand);
+
+        int sumOfDigitOfTwoPowerThousand = 0;
+        for (char c : twoPowerThousand.toString().toCharArray()) {
+            sumOfDigitOfTwoPowerThousand += Integer.parseInt(String.valueOf(c));
+        }
+
+        log.info("Sum of digit of two power thousand : {}", sumOfDigitOfTwoPowerThousand);
+    }
+
+}
--- src/test/java/com/zero/euler/EvenFibonacciNumberTest.java
+++ src/test/java/com/zero/euler/EvenFibonacciNumberTest.java
@@ -2,7 +2,11 @@ package com.zero.euler;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EvenFibonacciNumberTest {
 
@@ -10,5 +14,42 @@ class EvenFibonacciNumberTest {
     void testFindSumOfEvenFibonacciNumber() {
         int sum = EvenFibonacciNumber.findSumOfEvenFibonacciNumber();
         assertEquals(4_613_732, sum);
+    }
+
+    @Test
+    void test() {
+        String result = mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", new String[]{"hit"});
+        assertEquals("ball", result);
+
+        result = mostCommonWord("a.", new String[]{});
+        assertEquals("a", result);
+
+        result = mostCommonWord("a, a, a, a, b,b,b,c, c", new String[]{"a"});
+        assertEquals("b", result);
+    }
+
+    public String mostCommonWord(String paragraph, String[] banned) {
+        String[] words = paragraph.toLowerCase().split("\\W+");
+
+        Set<String> ban = new HashSet<>(Arrays.asList(banned));
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            if (!ban.contains(word)) {
+                map.merge(word, 1, Integer::sum);
+            }
+        }
+
+        int max = 0;
+        String result = "";
+        for (String key : map.keySet()) {
+            Integer count = map.get(key);
+            if (count > max) {
+                max = count;
+                result = key;
+            }
+        }
+
+        return result;
     }
 }
