1a424c43ffde65016354aa538447afbdb25319a5
Bobi Rival <bobirival@MacBook-Air-Bobi.local>
2025-07-23 12:34:53 +0700

    Update : solve lettice paths

---
+++ src/main/java/com/zero/euler/part0001/LatticePaths.java
@@ -0,0 +1,37 @@
+package com.zero.euler.part0001;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigInteger;
+
+@Slf4j
+public class LatticePaths {
+
+    public static void main(String[] args) {
+        log.info("grid 2 x 2 : {}", computeCombination(2));
+        log.info("grid 3 x 3 : {}", computeCombination(3));
+        log.info("grid 20 x 20 : {}", computeCombination(20));
+        log.info("grid 20 x 20 : {}", optimizedCombination(20));
+    }
+
+    public static BigInteger computeCombination(int gridLength) {
+        int pathLength = gridLength * 2;
+
+        BigInteger nFactorial = FactorialDigitSum.computeFactorial(pathLength);
+        BigInteger kFactorial = FactorialDigitSum.computeFactorial(gridLength);
+
+        return nFactorial.divide(kFactorial.multiply(kFactorial));
+    }
+
+    public static BigInteger optimizedCombination(int n) {
+        BigInteger numerator = BigInteger.ONE;
+        BigInteger denominator = FactorialDigitSum.computeFactorial(n);
+
+        for (int i = n + 1; i <= 2 * n; i++) {
+            numerator = numerator.multiply(BigInteger.valueOf(i));
+        }
+
+        return numerator.divide(denominator);
+    }
+
+}
